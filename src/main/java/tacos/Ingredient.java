92086c797b42ccfad05829c70179d20691ec2e81
vitalispopoff <vitalis.popoff@gmail.com>
2020-03-30 00:46:58 +0200

    2.1.2 controller (lecture source code broken)

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -8,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 public class Ingredient {
 
     private final String
-    id,
-    name;
+            id,
+            name;
     private final Type type;
 
     public /*static*/ enum Type {       // ? why static?
